09542aa4da21b55e9e081a4fe2c658bdee943253
Nikola Mitic <nmitic304@gmail.com>
2023-05-23 14:39:17 +0200

    Swimming-App Day 9

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import { useContext, useState } from "react";
 import "./login.scss";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      await login(inputs);
+    } catch (error) {
+      setErr(err.response.data)
+    }
+  };
+
   return (
     <div className="login">
       <div className="card">
@@ -22,9 +45,10 @@ const Login = () => {
         <div className="right">
           <h1>Login</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button>Login</button>
+            <input type="text" placeholder="Username" name="username" onChange={handleChange}/>
+            <input type="password" placeholder="Password" name="password" onChange={handleChange}/>
+            {err && err}
+            <button onClick={handleLogin}>Login</button>
           </form>
         </div>
       </div>
